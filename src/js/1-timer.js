37485754afc1c4b2c1f3d5a7ebd1f73db1067792
Dima <oxxTerm0s@gmail.com>
2024-11-24 17:15:55 +0200

    goit-js-hw-10

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -53,33 +53,18 @@ function updateCountdown() {
         const timeNow = new Date();
         const result = userSelectedDate - timeNow;
         if (result <= 0) {
-            clearInterval(interval);
+          clearInterval(interval);
+          inputTimer.disabled = false;
         } else {
             const { days, hours, minutes, seconds } = convertMs(result);
             const formattedDays = addLeadingZero(days);
     const formattedHours = addLeadingZero(hours);
     const formattedMinutes = addLeadingZero(minutes);
     const formattedSeconds = addLeadingZero(seconds);
-            timer.innerHTML = `
-            <div class="timer">
-      <div class="field">
-        <span class="value" data-days>${formattedDays}</span>
-        <span class="label">Days</span>
-      </div>
-      <div class="field">
-        <span class="value" data-hours>${formattedHours}</span>
-        <span class="label">Hours</span>
-      </div>
-      <div class="field">
-        <span class="value" data-minutes>${formattedMinutes}</span>
-        <span class="label">Minutes</span>
-      </div>
-      <div class="field">
-        <span class="value" data-seconds>${formattedSeconds}</span>
-        <span class="label">Seconds</span>
-      </div>
-    </div>
-    `;
+            timer.querySelector("[data-days]").textContent = formattedDays;
+timer.querySelector("[data-hours]").textContent = formattedHours;
+timer.querySelector("[data-minutes]").textContent = formattedMinutes;
+timer.querySelector("[data-seconds]").textContent = formattedSeconds;
     }
     }
     btn.addEventListener('click', () => {
